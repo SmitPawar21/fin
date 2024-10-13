c6892782f35d0ca8ab108f52de56cd3ab7c0b863
Smit Pawar <dynaster21@gmail.com>
2024-10-13 18:31:00 +0530

    update front and back

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,22 @@ const app = express()
 
 require('dotenv').config()
 
-const PORT = process.env.PORT
-
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: '',
+    method: ["POST", "GET", "DELETE"],
+    credentials: true
+}))
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
     db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
+    // app.listen(PORT, () => {
+    //     console.log('listening to port:', PORT)
+    // })
 }
 
 server()
